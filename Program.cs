1c0bb79df1eba86ed572b91caae01ecef1374e0d
Austin Sambrano <Austinsam210@gmail.com>
2023-12-22 17:17:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MLModel_ConsoleApp1.Services;
+
+while (true)
+{
+    Console.WriteLine("\nInput a name to guess their gender.");
+    string input = Console.ReadLine() ?? "John";
+    if (input == string.Empty || input.ToLower() == "quit")
+    {
+        break;
+    }
+    Console.Clear();
+    MLCon.MakePrediction(input);
+}
+
+Console.WriteLine("=============== End of process, hit any key to finish ===============");
+Console.ReadKey();
+
---
+++ Services/MLCon.cs
@@ -0,0 +1,40 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace MLModel_ConsoleApp1.Services
+{
+    public class MLCon
+    {
+        public static void MakePrediction(string input)
+        {
+            var sampleData = new MLModel.ModelInput()
+            {
+                Name = input
+            };
+
+            PrintResult(sampleData);
+        }
+
+        static void PrintResult(MLModel.ModelInput data)
+        {
+            Console.WriteLine("Using model to make single prediction...");
+
+            Console.WriteLine($"\nName: {data.Name}");
+
+            var results = MLModel.PredictAllLabels(data);
+
+            Console.WriteLine("\nResults:");
+            foreach (var score in results)
+            {
+                Console.WriteLine($"{score.Key,-40}{score.Value,-20}");
+            }
+            if (results.ToList()[0].Key == "M")
+            {
+                Console.WriteLine($"\nConclusion:\n{data.Name} is a Male");
+            } else
+            {
+                Console.WriteLine($"\nConclusion:\n{data.Name} is a Female");
+            }
+        }
+    }
+}
+
